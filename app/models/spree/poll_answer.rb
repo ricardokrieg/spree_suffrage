fc4244f015e48406e6d106648713290583b87713
Ricardo Franco <ricardo.krieg@gmail.com>
2016-04-18 00:25:15 -0300

    Update poll_answer.rb

--- app/models/spree/poll_answer.rb
+++ app/models/spree/poll_answer.rb
@@ -2,7 +2,7 @@ module Spree
   class PollAnswer < ActiveRecord::Base
     belongs_to :poll
     has_many :poll_votes, dependent: :destroy
-    attr_accessible :answer
+    #attr_accessible :answer
 
     #validates :answer, presence: true
     before_destroy :ensure_enclosing_poll_still_has_at_least_two_answers
